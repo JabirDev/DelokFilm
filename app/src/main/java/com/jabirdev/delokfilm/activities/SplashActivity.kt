de9689ea92f814e396fcc912cf28762abd0f817b
Taufan Fatahilah <taufan.fhm8@gmail.com>
2022-01-06 19:11:34 +0700

    Add SplashActivity and change color accent

---
+++ app/src/main/java/com/jabirdev/delokfilm/activities/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.jabirdev.delokfilm.activities
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+
+    }
+}
